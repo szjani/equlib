3582705e3b5631cff93a661fb1ea9f25df15b075
Szurovecz János <szjani@szjani.hu>
2011-06-07 22:17:22 +0200

    independent lib created from equcms

---
+++ tests/bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+define('APPLICATION_ENV', 'testing');
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+  '/development/Frameworks/zf1-classmap/library',
+)));
+
+require_once 'ZendX/Loader/AutoloaderFactory.php';
+ZendX_Loader_AutoloaderFactory::factory(array(
+  'ZendX_Loader_StandardAutoloader' => array(
+    'namespaces' => array(
+      'Equ'      => __DIR__ . '/../lib/Equ',
+      'Doctrine' => '/home/szjani/development/php/libs/doctrine2-orm/lib/Doctrine',
+      'Gedmo'    => '/home/szjani/development/php/libs/l3pp4rd/DoctrineExtensions/lib/Gedmo',
+      'DoctrineExtensions' => '/home/szjani/development/php/libs/beberlei/DoctrineExtensions/lib/DoctrineExtensions',
+    ),
+    'fallback_autoloader' => true,
+  ),
+));
