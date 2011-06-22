3e62c6fc855add0bb24dc4c0cd04491f4cd2a8d7
Szurovecz János <szjani@szjani.hu>
2011-06-23 01:38:15 +0200

    Performance increase in config loading and bootstrapping

--- lib/Equ/Application/Bootstrap/Bootstrap.php
+++ lib/Equ/Application/Bootstrap/Bootstrap.php
@@ -7,22 +7,45 @@ use
 
 class Bootstrap extends \Zend_Application_Bootstrap_Bootstrap {
 
+  private function getCache() {
+    $cache = \Zend_Cache::factory(
+      'File',
+      'File',
+      array(// Frontend Default Options
+        'master_files' => array(
+          APPLICATION_PATH . '/configs/production.xml',
+          APPLICATION_PATH . '/configs/development.xml',
+        ),
+        'automatic_serialization' => true
+      ),
+      array(// Backend Default Options
+        'cache_dir' => APPLICATION_PATH . '/../data/cache'
+      )
+    );
+    return $cache;
+  }
+  
   public function getContainer() {
     $options = $this->getOption('bootstrap');
 
     if (null === $this->_container && $options['container']['type'] == 'symfony') {
       $container = null;
-      $name = 'Container'.md5($this->getEnvironment()).'ServiceContainer';
+      $name = 'Container'.$this->getEnvironment().'ServiceContainer';
       $file = APPLICATION_PATH.'/../data/cache/'.$name.'.php';
-      if ($this->getEnvironment() !== 'development' && file_exists($file)) {
+      
+      $cache = $this->getCache();
+      $diContainerLoaded = $cache->load('DIContainerLoaded');
+      if (!$diContainerLoaded) {
+        $cache->save('loaded', 'DIContainerLoaded');
+      }
+      
+      if ($diContainerLoaded && file_exists($file)) {
         require_once $file;
         $container = new $name();
       } else {
         $container = ServiceContainerFactory::getContainer($options['container']);
-        if ($this->getEnvironment() !== 'development') {
-          $dumper = new DependencyInjection\Dumper\PhpDumper($container);
-          file_put_contents($file, $dumper->dump(array('class' => $name)));
-        }
+        $dumper = new DependencyInjection\Dumper\PhpDumper($container);
+        file_put_contents($file, $dumper->dump(array('class' => $name)));
       }
       $this->_container = $container;
       \Zend_Controller_Action_HelperBroker::addHelper(new ServiceContainer());
