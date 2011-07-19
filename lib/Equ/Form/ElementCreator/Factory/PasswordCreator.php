f14de8089c82e016224840a3e850f7a5ac8e9339
János Szurovecz <janos.szurovecz@factory.co.hu>
2011-07-19 09:08:16 +0200

    use template

--- lib/Equ/Form/ElementCreator/Factory/PasswordCreator.php
+++ lib/Equ/Form/ElementCreator/Factory/PasswordCreator.php
@@ -4,7 +4,15 @@ namespace Equ\Form\ElementCreator\Factory;
 class PasswordCreator extends \Equ\Form\ElementCreator\AbstractCreator {
 
   protected function buildElement($fieldName) {
-    return new \Zend_Form_Element_Password($fieldName);
+    $element = new \Zend_Form_Element_Password($fieldName);
+    $element->getView()->addBasePath(__DIR__ . '/view');
+    $element->addDecorators(array(
+      array('ViewScript', array(
+        'placement' => false,
+        'viewScript' => 'default.phtml',
+      ))
+    ));
+    return $element;
   }
   
 }
