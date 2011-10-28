a5cc46e80e61f4dad91137e04afb664930e13c29
János Szurovecz <janos.szurovecz@factory.co.hu>
2011-10-28 10:08:10 +0200

    authenticated user is not always available in Bootstrap.php, navigationHelper->setRole() must be in the plugin

--- lib/Equ/Controller/Plugin/Navigation.php
+++ lib/Equ/Controller/Plugin/Navigation.php
@@ -3,15 +3,20 @@ namespace Equ\Controller\Plugin;
 use
   Zend_Cache_Core,
   Zend_Navigation,
+  Zend_Acl,
+  Zend_View,
   Equ\Navigation\Item as NavigationItem,
-  Equ\Navigation\ItemRepository as NavigationItemRepository;
+  Equ\Navigation\ItemRepository as NavigationItemRepository,
+  Symfony\Component\DependencyInjection\Container;
 
 class Navigation extends \Zend_Controller_Plugin_Abstract {
 
+  const KEY = 'navigation';
+  
   /**
-   * @var Zend_Navigation
+   * @var Container
    */
-  private $navigation;
+  private $container;
   
   /**
    *
@@ -26,29 +31,63 @@ class Navigation extends \Zend_Controller_Plugin_Abstract {
   private $cache;
   
   /**
-   * @param Zend_Navigation $navigation
-   * @param NavigationItemRepository $itemRepo
-   * @param Zend_Cache_Core $cache 
+   * @var Zend_Acl 
    */
-  public function __construct(Zend_Navigation $navigation, NavigationItemRepository $itemRepo, Zend_Cache_Core $cache) {
-    $this->navigation = $navigation;
+  private $acl;
+  
+  /**
+   * @var Zend_View 
+   */
+  private $view;
+  
+  /**
+   * @param Container $container
+   * @param NavigationItemRepository $itemRepo
+   * @param Zend_Cache_Core $cache
+   * @param Zend_Acl $acl
+   * @param Zend_View $view 
+   */
+  public function __construct(
+    Container $container,
+    NavigationItemRepository $itemRepo,
+    Zend_Cache_Core $cache,
+    Zend_Acl $acl,
+    Zend_View $view
+  ) {
+    $this->container  = $container;
     $this->itemRepo   = $itemRepo;
     $this->cache      = $cache;
+    $this->acl        = $acl;
+    $this->view       = $view;
   }
   
   public function routeShutdown(\Zend_Controller_Request_Abstract $request) {
     if ($request->getParam('format') == 'ajax') {
       return;
     }
-    /* @var $item NavigationItem */
-    foreach ($this->itemRepo->getNavigationItems() as $item) {
-      if (false !== strpos((string)$item->getNavigationPage()->getResource(), 'update')) {
-        $item->getNavigationPage()->setVisible(false);
+    
+    $navigation = $this->container->get(self::KEY);
+    $navigationCache = $this->cache->load(self::KEY);
+    if (false !== $navigationCache) {
+      $navigation = $navigationCache;
+    } else {
+      /* @var $item NavigationItem */
+      foreach ($this->itemRepo->getNavigationItems() as $item) {
+        if (false !== strpos((string)$item->getNavigationPage()->getResource(), 'update')) {
+          $item->getNavigationPage()->setVisible(false);
+        }
+        $parentNav = $item->getParent() ? $item->getParent()->getNavigationPage() : $navigation;
+        $parentNav->addPage($item->getNavigationPage());
       }
-      $parentNav = $item->getParent() ? $item->getParent()->getNavigationPage() : $this->navigation;
-      $parentNav->addPage($item->getNavigationPage());
+      $this->cache->save($navigation, self::KEY);
     }
-    $this->cache->save($this->navigation, 'navigation');
+    $this->container->set(self::KEY, $navigation);
+    
+    $navigationHelper = $this->view->getHelper('navigation');
+    $navigationHelper
+      ->setContainer($navigation)
+      ->setAcl($this->acl)
+      ->setRole(\Zend_Auth::getInstance()->getIdentity());
   }
 
 }
