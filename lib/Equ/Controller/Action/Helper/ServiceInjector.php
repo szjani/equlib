c8c17395ef84db48f29aa3c7be714241b1070a1a
János Szurovecz <janos.szurovecz@factory.co.hu>
2012-01-30 13:25:08 +0100

    Autoinject services from DIC to ActionController

---
+++ lib/Equ/Controller/Action/Helper/ServiceInjector.php
@@ -0,0 +1,31 @@
+<?php
+namespace Equ\Controller\Action\Helper;
+use \Symfony\Component\DependencyInjection\Container;
+
+class ServiceInjector extends \Zend_Controller_Action_Helper_Abstract {
+  
+  /**
+   * @var Container
+   */
+  private $container;
+  
+  /**
+   * @param Container $container 
+   */
+  public function __construct(Container $container) {
+    $this->container = $container;
+  }
+  
+  public function init() {
+    $filter = new \Zend_Filter_Word_CamelCaseToSeparator('.');
+    foreach (get_object_vars($this->_actionController) as $property => $variable) {
+      if (!isset($variable)) {
+        $filteredValue = $filter->filter($property);
+        if ($this->container->has($filteredValue)) {
+          $this->_actionController->$property = $this->container->get($filteredValue);
+        }
+      }
+    }
+  }
+  
+}
