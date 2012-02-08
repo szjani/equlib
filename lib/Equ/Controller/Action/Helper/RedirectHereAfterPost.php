bd607b0eddc3fcd64578d41d6ab500915f11e9ab
Szurovecz János <szjani@szjani.hu>
2012-02-08 22:22:36 +0100

    url is checkable

--- lib/Equ/Controller/Action/Helper/RedirectHereAfterPost.php
+++ lib/Equ/Controller/Action/Helper/RedirectHereAfterPost.php
@@ -15,6 +15,8 @@ class RedirectHereAfterPost extends \Zend_Controller_Action_Helper_Abstract {
   
   const DEFAULT_KEY = 'redirect';
   
+  protected $hasUrl = false;
+  
   private $key = self::DEFAULT_KEY;
   
   public function setKey($key) {
@@ -29,11 +31,21 @@ class RedirectHereAfterPost extends \Zend_Controller_Action_Helper_Abstract {
     $this->saveCurrentUrl();
   }
   
+  public function hasUrl() {
+    return $this->hasUrl;
+  }
+  
   public function saveCurrentUrl() {
     $router = $this->getActionController()->getFrontController()->getRouter();
     if ($router instanceof \Zend_Controller_Router_Rewrite) {
       $router->setGlobalParam($this->getKey(), base64_encode($this->getRequest()->getRequestUri()));
+      $this->hasUrl = true;
     }
+  }
+  
+  public function preDispatch() {
+    $encodedRedirectUrl = $this->getRequest()->getParam($this->getKey());
+    $this->hasUrl = isset($encodedRedirectUrl);
   }
   
   public function postDispatch() {
