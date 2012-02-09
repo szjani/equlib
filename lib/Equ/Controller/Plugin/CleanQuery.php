a756f5ea6f44dbcc5d80994767df4fa1fb389808
János Szurovecz <janos.szurovecz@factory.co.hu>
2012-02-09 10:40:10 +0100

    invalid filtering in case of ajax requests

--- lib/Equ/Controller/Plugin/CleanQuery.php
+++ lib/Equ/Controller/Plugin/CleanQuery.php
@@ -4,15 +4,13 @@ namespace Equ\Controller\Plugin;
 class CleanQuery extends \Zend_Controller_Plugin_Abstract {
 
   public function routeShutdown(\Zend_Controller_Request_Abstract $request) {
-    if (!$request->isXmlHttpRequest()) {
-      $paramHandler = new \Equ\Controller\ArrayParamHandler();
-      if (preg_match('/\?/', $request->getRequestUri())) {
-        $newParams = $paramHandler->convertArrayToString($request->getParams());
-        \Zend_Controller_Action_HelperBroker::getStaticHelper('Redirector')->gotoRouteAndExit($newParams);
-      } else {
-        $newParams = $paramHandler->convertStringToArray($request->getParams());
-        $request->setParams(array_merge($newParams, $request->getParams()));
-      }
+    $paramHandler = new \Equ\Controller\ArrayParamHandler();
+    if (preg_match('/\?/', $request->getRequestUri())) {
+      $newParams = $paramHandler->convertArrayToString($request->getParams());
+      \Zend_Controller_Action_HelperBroker::getStaticHelper('Redirector')->gotoRouteAndExit($newParams);
+    } else {
+      $newParams = $paramHandler->convertStringToArray($request->getParams());
+      $request->setParams(array_merge($newParams, $request->getParams()));
     }
   }
 
