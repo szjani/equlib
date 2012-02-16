1ba8ab92ea47568b453e25edb23312b6c1edc203
János Szurovecz <janos.szurovecz@factory.co.hu>
2012-02-16 16:28:53 +0100

    too big textarea

--- lib/Equ/Form/ElementCreator/Bootstrap/TextCreator.php
+++ lib/Equ/Form/ElementCreator/Bootstrap/TextCreator.php
@@ -5,6 +5,7 @@ class TextCreator extends BaseCreator {
 
   protected function buildElement($fieldName) {
     $element = new \Zend_Form_Element_Textarea($fieldName);
+    $element->setAttrib('rows', 6);
     $this->initDecorators($element);
     return $element;
   }
