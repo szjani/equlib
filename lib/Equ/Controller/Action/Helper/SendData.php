e4ba09772885503ed2eab8664902ef02e5681fc6
János Szurovecz <janos.szurovecz@factory.co.hu>
2012-02-28 10:19:57 +0100

    sendFile and sendData helpers

---
+++ lib/Equ/Controller/Action/Helper/SendData.php
@@ -0,0 +1,33 @@
+<?php
+namespace Equ\Controller\Action\Helper;
+
+/**
+ * @license     http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @author      Szurovecz János <szjani@szjani.hu>
+ */
+class SendData extends SendContent {
+  
+  /**
+   * @param type $data
+   * @param array $options 
+   */
+  public function direct($data, array $options = array()) {
+    $this->sendData($data, $options);
+  }
+  
+  /**
+   * @param type $data
+   * @param array $options 
+   */
+  public function sendData($data, array $options = array()) {
+    $this
+      ->setFileName(md5(time() . mt_rand()))
+      ->setModified(time())
+      ->setContentLength(strlen($data))
+      ->setContentType('application/octet-stream')
+      ->setOptions($options)
+      ->sendHeaders();
+    $this->getResponse()->setBody((string)$data);
+  }
+  
+}
---
+++ lib/Equ/Controller/Action/Helper/SendFile.php
@@ -0,0 +1,84 @@
+<?php
+namespace Equ\Controller\Action\Helper;
+use SplFileInfo;
+use
+  InvalidArgumentException,
+  RuntimeException;
+
+/**
+ * @license     http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @author      Szurovecz János <szjani@szjani.hu>
+ */
+class SendFile extends SendContent {
+  
+  /**
+   * @param SplFileInfo $file
+   * @param array $options 
+   */
+  public function direct(SplFileInfo $file, array $options = array()) {
+    $this->sendFile($file, $options);
+  }
+  
+  /**
+   * @param type $filename
+   * @return string 
+   */
+  private function detectMimeType($filename) {
+    $result = null;
+    
+    if (class_exists('finfo', false)) {
+      $const = defined('FILEINFO_MIME_TYPE') ? FILEINFO_MIME_TYPE : FILEINFO_MIME;
+      $mime = @finfo_open($const);
+      if (!empty($mime)) {
+        $result = finfo_file($mime, $filename);
+      }
+      unset($mime);
+    }
+
+    if (empty($result) && (function_exists('mime_content_type') && ini_get('mime_magic.magicfile'))) {
+      $result = mime_content_type($filename);
+    }
+
+    if (empty($result)) {
+      $result = 'application/octet-stream';
+    }
+    return $result;
+  }
+  
+  /**
+   * @param SplFileInfo $file
+   * @param array $options
+   * @throws InvalidArgumentException
+   * @throws RuntimeException 
+   */
+  public function sendFile(SplFileInfo $file, array $options = array()) {
+    $this
+      ->setFileName($file->getFilename())
+      ->setModified($file->getMTime())
+      ->setContentLength($file->getSize())
+      ->setContentType($this->detectMimeType($file->getPathname()))
+      ->setOptions($options)
+      ->sendHeaders();
+    
+    ob_end_clean();
+    ignore_user_abort(1);
+    if (!ini_get('safe_mode')) {
+      set_time_limit(0);
+    }
+    
+    $fp = fopen($file->getPathname(), 'rb');
+    if (!$fp) {
+      throw new InvalidArgumentException('Invalid filename: '.$file->getPathname());
+    }
+    while (!feof($fp)) {
+      print fread($fp, 1024);
+      if (connection_aborted()) {
+        fclose($fp);
+        throw new RuntimeException('Unsuccess file download: '.$file->getPathname());
+      }
+    }
+    fclose($fp);
+    $this->getResponse()->sendResponse();
+  }
+
+}
