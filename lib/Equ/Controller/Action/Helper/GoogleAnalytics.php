dbc7c806875db63c71d6ef7d1e73922c28d2f33b
János Szurovecz <janos.szurovecz@factory.co.hu>
2012-03-13 11:09:01 +0100

    can be disabled (eg on dev env)

--- lib/Equ/Controller/Action/Helper/GoogleAnalytics.php
+++ lib/Equ/Controller/Action/Helper/GoogleAnalytics.php
@@ -6,13 +6,21 @@ class GoogleAnalytics extends Zend_Controller_Action_Helper_Abstract {
   
   protected $code;
   
-  public function __construct($code) {
-    $this->code = $code;
+  protected $enabled;
+  
+  /**
+   * @param string $code
+   * @param boolean $enabled 
+   */
+  public function __construct($code, $enabled = true) {
+    $this->code    = $code;
+    $this->enabled = $enabled;
   }
   
   public function preDispatch() {
-    $view = $this->getActionController()->view;
-    $view->headScript()->appendScript("
+    if ($this->enabled) {
+      $view = $this->getActionController()->view;
+      $view->headScript()->appendScript("
         var _gaq = _gaq || [];
         _gaq.push(['_setAccount', '{$this->code}']);
         _gaq.push(['_trackPageview']);
@@ -22,7 +30,8 @@ class GoogleAnalytics extends Zend_Controller_Action_Helper_Abstract {
           ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
           var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
         })();
-    ");
+      ");
+    }
   }
   
 }
