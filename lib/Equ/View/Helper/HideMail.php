b8645d056b1e5daa8aa61b5af1f81fe7030285bf
János Szurovecz <janos.szurovecz@factory.co.hu>
2012-03-21 13:42:55 +0100

    simple email hiding

---
+++ lib/Equ/View/Helper/HideMail.php
@@ -0,0 +1,14 @@
+<?php
+namespace Equ\View\Helper;
+use
+  Zend_View_Helper_Abstract;
+
+class HideMail extends Zend_View_Helper_Abstract {
+  
+  public function hideMail($email) {
+    $email = str_replace('@', '&#x' . base_convert(ord('@'), 10, 16) . ';', $email);
+    $email = str_replace('.', '&#x' . base_convert(ord('.'), 10, 16) . ';', $email);
+    return $email;
+  }
+  
+}
