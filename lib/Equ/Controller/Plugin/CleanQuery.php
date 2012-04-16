819a0170616514643ac3535032b663902b17f7d5
Szurovecz János <szjani@szjani.hu>
2012-04-16 20:57:58 +0200

    disable URL rewrita in case of POST request

--- lib/Equ/Controller/Plugin/CleanQuery.php
+++ lib/Equ/Controller/Plugin/CleanQuery.php
@@ -6,14 +6,16 @@ class CleanQuery extends \Zend_Controller_Plugin_Abstract {
   public function routeShutdown(\Zend_Controller_Request_Abstract $request) {
     $paramHandler = new \Equ\Controller\ArrayParamHandler();
     if (preg_match('/\?/', $request->getRequestUri())) {
-      $filter    = new \Zend_Filter_Null(\Zend_Filter_Null::STRING);
-      $newParams = array_filter(
-        $paramHandler->convertArrayToString($request->getParams()),
-        function($value) use ($filter) {
-          return $filter->filter($value) !== null;
-        }
-      );
-      \Zend_Controller_Action_HelperBroker::getStaticHelper('Redirector')->gotoRouteAndExit($newParams, null, true);
+      if (!$request->isPost()) {
+        $filter    = new \Zend_Filter_Null(\Zend_Filter_Null::STRING);
+        $newParams = array_filter(
+          $paramHandler->convertArrayToString($request->getParams()),
+          function($value) use ($filter) {
+            return $filter->filter($value) !== null;
+          }
+        );
+        \Zend_Controller_Action_HelperBroker::getStaticHelper('Redirector')->gotoRouteAndExit($newParams, null, true);
+      }
     } else {
       $newParams = $paramHandler->convertStringToArray($request->getParams());
       $request->setParams(array_merge($newParams, $request->getParams()));
