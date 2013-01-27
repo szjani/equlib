337797e462522448383244195e76233ffc77b55f
Szurovecz János <szjani@szjani.hu>
2013-01-27 20:20:53 +0100

    composer support

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,54 +1,11 @@
 <?php
-define('APPLICATION_ENV', 'testing');
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    include_once __DIR__ . '/../vendor/autoload.php';
+}
 
-// ==== Modify these values! ==== //
+define('APPLICATION_ENV', 'testing');
 define('PROJECT_CACHE_PREFIX', 'equlib_');
 
-$zfDir         = '/development/Frameworks/ZF_1.11_svn';
-$doctrineDir   = '/development/Frameworks/Doctrine-2.2';
-$fixturesDir   = '/development/Frameworks/doctrine-fixtures';
-$migrationsDir = '/development/Frameworks/migrations';
-// ============================== //
+use Doctrine\Common\Annotations\AnnotationRegistry;
 
-$zfDebugDir    = __DIR__ . '/../library/ZFDebug/library';
-$zfDir .= '/library';
-$sources = array(
-  'Zend'     => array($zfDir, '_'),
-  'ZFDebug'  => array($zfDebugDir, '_'),
-  'Doctrine\ORM' => $doctrineDir . '/lib',
-  'Doctrine\Common\DataFixtures' => $fixturesDir . '/lib',
-  'Doctrine\DBAL\Migrations'     => $migrationsDir . '/lib',
-  'Doctrine\Common' => $doctrineDir . '/lib/vendor/doctrine-common/lib',
-  'Doctrine\DBAL'   => $doctrineDir . '/lib/vendor/doctrine-dbal/lib',
-  'Gedmo'    => __DIR__ . '/../library/Gedmo/lib',
-  'Equ'      => __DIR__ . '/../lib',
-  'Symfony'  => __DIR__ . '/../library',
-);
-
-set_include_path(implode(PATH_SEPARATOR, array(
-  $zfDir, $zfDebugDir, get_include_path()
-)));
-
-require_once __DIR__ . '/../library/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-$loader = new UniversalClassLoader();
-$loader->register();
-
-foreach ($sources as $namespace => $source) {
-  $dir = $source;
-  $separator = '\\';
-  if (is_array($source)) {
-    $dir = array_shift($source);
-    if (!empty($source)) {
-      $separator = array_shift($source);
-      $loader->registerPrefix($namespace, $dir);
-    }
-  } else {
-    $loader->registerNamespace($namespace, $dir);
-  }
-}
-Doctrine\Common\Annotations\AnnotationRegistry::registerAutoloadNamespace(
-  'Doctrine\ORM\Mapping',
-  $sources['Doctrine\ORM']
-);
+AnnotationRegistry::registerFile(__DIR__ . "/../vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php");
